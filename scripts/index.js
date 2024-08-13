1a474ebd05b470f800d2464d22cde947ac2af81e
iliberato dev <118570733+iliberato-dev@users.noreply.github.com>
2024-08-13 12:02:41 -0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -2,17 +2,25 @@ const navbar = document.querySelector('.navbar');
 const mobileNavbar = document.querySelector('.navbar__mobile');
 const button = document.querySelector('.burguer');
 
-button.addEventListener('click', ()=>{
+button.addEventListener('click', () => {
     mobileNavbar.classList.toggle('active');
+
+    if (mobileNavbar.classList.contains('active')) {
+        button.src = 'https://cdn-icons-png.flaticon.com/512/1/1766.png'
+    } else {
+        button.src = '../assets/menu.svg'
+    }
+
+
+
+
 });
 
-window.addEventListener('scroll', ()=>{
-   if (this.window.pageYOffset > 0) {
-    navbar.classList.add('active');
-   
-   } 
-   else{
-    navbar.classList.remove('active');
-   }
+window.addEventListener('scroll', () => {
+    if (this.window.pageYOffset > 0) {
+        mobileNavbar.classList.remove('active');
+        button.src = '../assets/menu.svg'
+
+    }
 })
 
